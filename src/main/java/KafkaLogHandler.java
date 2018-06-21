890f9a5d6df1d8040eb6760afac560371b82c164
Kimheng <kimheng@Kimhengs-MacBook-Pro.local>
2018-06-21 15:47:16 +0700

    Convert KafkaLogHandler to Json Format

--- src/main/java/KafkaLogHandler.java
+++ src/main/java/KafkaLogHandler.java
@@ -1,12 +1,19 @@
 
+import com.google.gson.Gson;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+
+import java.io.FileWriter;
+import java.util.*;
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.codehaus.jackson.JsonNode;
+
+
 
 
 public class KafkaLogHandler extends Handler {
@@ -15,46 +22,59 @@ public class KafkaLogHandler extends Handler {
 
     public void publish(LogRecord logRecord) {
 
-         List<String> handler = new ArrayList<String>();
+        List<String> handler = new ArrayList<String>();
 
 
         handler.add(logRecord.getLevel() + ":" + logRecord.getMillis() + "*" + logRecord.getSourceClassName() + "&" + logRecord.getSourceMethodName() + " <" + logRecord.getMessage() + ">");
 
 
-        System.out.println(handler);
+//        System.out.println(handler);
+        String res = String.join(",", handler);
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("status", "200");
+        map.put("data", res);
+        String json = null;
+        try {
+            json = mapper.writeValueAsString(map);
 
-        Properties properties = new Properties();
+            Properties properties = new Properties();
 
-        // kafka bootstrap server
-        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
-        properties.setProperty("key.serializer", StringSerializer.class.getName());
-        properties.setProperty("value.serializer", StringSerializer.class.getName());
-
-        // producer acks
-        properties.setProperty("acks", "1");
-        properties.setProperty("retries", "3");
-        properties.setProperty("linger.ms", "1");
+             //kafka bootstrap server
+            String TOPIC = "testing1";
+            properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+            properties.setProperty("key.serializer", StringSerializer.class.getName());
+            properties.setProperty("value.serializer", StringSerializer.class.getName());
 
 
-        Producer<String, String> producer = new org.apache.kafka.clients.producer.KafkaProducer<String, String>(properties);
+            // producer acks
+            properties.setProperty("acks", "1");
+            properties.setProperty("retries", "3");
+            properties.setProperty("linger.ms", "1");
 
 
-            String res = String.join(",", handler);
-            ProducerRecord<String, String> productRecord = new ProducerRecord<String, String>("testing1", "3", res);
+            Producer<String, String > producer = new org.apache.kafka.clients.producer.KafkaProducer<String, String >(properties);
+
+
+            ProducerRecord<String, String > productRecord = new ProducerRecord<String, String>(TOPIC,"3",json);
             producer.send(productRecord);
 
-        producer.close();
+            producer.close();
 
-
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        System.out.println(json);
 
     }
 
+    @Override
     public void flush() {
-    }
-
-    public void close() {
 
     }
 
+    @Override
+    public void close() throws SecurityException {
 
+    }
 }
